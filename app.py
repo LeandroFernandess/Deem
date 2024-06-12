f817c2fe0ec8ab4b7b75749eaa0cec3f1fa053f9
Leandro Fernandes <137462325+LeandroFernandess@users.noreply.github.com>
2024-06-11 22:58:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 
 import streamlit as st
 from forms.formulario import form
-from table.tabela import tabela
-from streamlit_navigation_bar import st_navbar
+from table.tabela import visualizar_dados
 
-opção_selecionada = st_navbar(["Formulário de Deem 📝", "Visão Geral 📊"])
+
+opção_selecionada = st.sidebar.radio(label='Opções', options=["Formulário de Deem 📝", "Visão Geral 📊"])
 
 # Exibindo o conteúdo dependendo da opção selecionada:
 
 if opção_selecionada == "Formulário de Deem 📝":
     form()
 if opção_selecionada == "Visão Geral 📊":
-    tabela()
+    visualizar_dados()
